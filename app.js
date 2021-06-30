316b583efc66bf44dc9fdf629355dd93e171f210
unknown <harsimran.sliet@gmail.com>
2021-07-01 00:06:32 +0530

    okay

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-// Import Modules
 require('dotenv').config();
-const express = require('express');;
+require('./db/index');
 
+// Import Modules
+const express = require('express');;
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+
 // Import Files
-<<<<<<< HEAD
-import './db/index';
-import authRoutes from './routes/auth'
-import userRoutes from './routes/user'
-import domainRoutes from './routes/domain'
-import coordinatorRoutes from './routes/coordinator'
-import path from "path"
+const path = require("path");
 const sponsorRoutes = require('./routes/sponsor');
-=======
-require('./db/index');
 const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
 const domainRoutes = require('./routes/domain')
@@ -22,11 +16,11 @@ const coordinatorRoutes = require('./routes/coordinator')
 const sponsorRoutes = require('./routes/sponsor')
 const eventsRoutes = require('./routes/event')
 
->>>>>>> 5235f94378089c75f695eb5029a50ce12a5f37d7
 // Constatns 
 const PORT = process.env.PORT || 4000; //Server Port
 const app = express();
 
+app.use(express.static(path.join(__dirname, "uploads")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cookieParser());
@@ -37,14 +31,9 @@ app.use("/api", authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', domainRoutes);
 app.use('/api', coordinatorRoutes);
-<<<<<<< HEAD
-app.use('/api', sponsorRoutes);
 // app.use('/uploads', express.static(path.join(__dirname, "uploads")));
-app.use(express.static(path.join(__dirname, "uploads")));
-=======
 app.use('/api', eventsRoutes);
 app.use('/api', sponsorRoutes);
->>>>>>> 5235f94378089c75f695eb5029a50ce12a5f37d7
 app.use('//', (req, res) => {
     res.send('Welcome :)')
 });
