6400ff19bee527741fd1e00bfd0ae159aac76f42
Amitojasa <singh99amitoj@gmail.com>
2021-07-01 00:40:45 +0530

    file upload separate function

---
+++ utills/fileupload.js
@@ -0,0 +1,25 @@
+const formidable = require("formidable");
+const fs = require("fs")
+exports.uploadFileFunc = (file) => {
+
+    if (file.photo.size > 3000000) {
+        return json({
+            error: "File size too big!"
+        })
+    }
+    var oldPath = file.photo.path;
+    var newPath1 = Date.now() + '.' + file.photo.name.split('.').pop();
+    var newPath = 'uploads/' + newPath1
+    var rawData = fs.readFileSync(oldPath)
+
+    fs.writeFile(newPath, rawData, function (err) {
+        if (err) {
+
+            return json({
+                error: err
+            })
+        }
+
+    })
+    return newPath1;
+}
