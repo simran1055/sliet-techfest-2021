a48de86086cb44126cd808ad0d1f731fcd2048cd
Amitojasa <singh99amitoj@gmail.com>
2021-07-12 23:25:59 +0530

    workshop

---
+++ models/workshopSession.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const workshopSessionSchema = new mongoose.Schema({
+
+    // Date and time, Scheduled Link, Serial Number
+})
+
+
+module.exports = mongoose.model("WorkshopSession", workshopSessionSchema)
