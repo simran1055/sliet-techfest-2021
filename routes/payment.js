32b2fdf05a36df19721ef4ec1b4b14f9ef2868cf
unknown <harsimran.sliet@gmail.com>
2021-08-04 05:49:05 +0530

    update

--- routes/payment.js
+++ routes/payment.js
@@ -2,7 +2,6 @@ const express = require('express')
 const { getUserById, getUser, updateUser, notify } = require("../controllers/user")
 const { isAuthenticated, isSignedIn, isSuperAdmin } = require("../controllers/auth")
 const { getPaymentHashId, processPayment, stripePayment, payStripeLast,paymentSuccessAction } = require('../controllers/payment')
-const stripe = require('stripe')('sk_test_51ILhyyK2faB59yIQvGfqF2WwmNNjgh8EnKlpe5XrhsEuOaujLeABgt5p4VBCcXJso4s35aK3j4NMMMQDWWEduEYT00DicJ00h0');
 var router = express.Router()
 
 router.param('userId', getUserById);
